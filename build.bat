839bb97576f3c0a860608a81e7fe2bfc6ad244dd
赖柄沣 <bingfengdev@aliyun.com>
2021-01-29 22:40:53 +0800

    添加对象存储服务

--- build.bat
+++ build.bat
@@ -1,1 +1,3 @@
+rmdir /s/q dist
+rmdir /s/q logs
 mvn clean package -P prod
---
+++ yeju-service-provider/yeju-oos-service-provider/script/start.sh
@@ -0,0 +1,1 @@
+nohup java -Xms128m -Xmx256m -jar ../dist/yeju-oos-service-provider-1.0.0.jar >> /data/yeju/yeju-oos-service-provider-1.0.0/log/start.log 2>&1 &

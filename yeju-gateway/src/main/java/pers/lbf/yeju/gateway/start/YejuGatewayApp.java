735f01f3d4523910952dd45478150225af9a324b
赖柄沣 <bingfengdev@aliyun.com>
2021-03-08 15:45:31 +0800

    同步Prometheus配置

--- yeju-gateway/src/main/java/pers/lbf/yeju/gateway/start/YejuGatewayApp.java
+++ yeju-gateway/src/main/java/pers/lbf/yeju/gateway/start/YejuGatewayApp.java
@@ -1,5 +1,6 @@
 package pers.lbf.yeju.gateway.start;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -10,12 +11,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020/11/26 1:06
  */
 @SpringBootApplication(scanBasePackages = "pers.lbf.yeju.gateway")
+@Slf4j
 public class YejuGatewayApp {
 
     public static void main(String[] args) {
-        SpringApplication.run(YejuGatewayApp.class,args);
-    }
+        SpringApplication.run(YejuGatewayApp.class, args);
+        log.info("############################################");
+        log.info("#------------  微服务网关启动成功！ -----------#");
+        log.info("############################################");
 
+    }
 
 
 }
